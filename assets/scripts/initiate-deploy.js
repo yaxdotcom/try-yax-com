13bf32463718bb8bf881cc16c5818b54685d38aa
Daniel Kehoe <daniel@danielkehoe.com>
2020-08-14 14:12:08 +0800

    rewrite 'initiate-deploy.js' bc Safari does not support FormData Event

--- assets/scripts/initiate-deploy.js
+++ assets/scripts/initiate-deploy.js
@@ -21,23 +21,10 @@ const url = new URL('https://github.com/login/oauth/authorize')
 // capture form data after 'deploy now' button is clicked
 form.addEventListener("submit", function (event) {
     event.preventDefault()
-    new FormData(form)
-})
-
-// reset form after 'reset form' link is clicked
-reset_link.addEventListener("click", function () {
-    form.reset()
-    deploy_button.style.display = 'block'
-    for (var i = 0; i < confirmable.length; i ++) {
-        confirmable[i].style.display = 'none'
-    }
-})
-
-// create a URL that includes form data in a 'state' query parameter
-form.addEventListener("formdata", event => {
+    let formData = new FormData(form)
     const data = {}
-    for(let [name, value] of event.formData) {
-        data[name] = value
+    for(let [name, value] of formData) {
+      data[name] = value
     }
     const encoded_state = btoa(JSON.stringify(data))
     url.searchParams.set('client_id', GITHUB_CLIENT_ID)
@@ -54,6 +41,15 @@ form.addEventListener("formdata", event => {
     }
 })
 
+// reset form after 'reset form' link is clicked
+reset_link.addEventListener("click", function () {
+    form.reset()
+    deploy_button.style.display = 'block'
+    for (var i = 0; i < confirmable.length; i ++) {
+        confirmable[i].style.display = 'none'
+    }
+})
+
 // display the 'confirm button' after form data is captured
 function confirm(data) {
     repo_name.innerHTML = data['repository']
@@ -67,7 +63,7 @@ function confirm(data) {
 // display pageloader after 'confirm' button is clicked
 confirm_button.addEventListener("click", function () {
     spinner.classList.add('is-active')
-    // URL for GitHub authentication containing form data hidden in a 'state' query parameter 
+    // URL for GitHub authentication containing form data hidden in a 'state' query parameter
     window.open(url, "_self")
     event.preventDefault()
 })
